9bdf37f7f7f810c580a85befc3bd9b1292777566
Predeactor <61093863+Predeactor@users.noreply.github.com>
2020-06-04 22:54:19 +0200

    Learning EoL & Update (#2)
    * Update learning.py
    
    * Create info.json
    
    * Update README.md

--- README.md
+++ README.md
@@ -6,27 +6,35 @@
 
 This is a repo made for [Red](https://github.com/Cog-Creators/Red-DiscordBot).
 
-# Again news! (13/5/2020)
-Oh hey you, I hope you're doing well! You may wonder why I still didn't made the birthday and mail cog? Well, I just lose the motivation about them (*I didn't even start to write them*), but still, now I got 2 real project :
-- A game where you should help someone, an interactive game made by me*TM*.
-- A cog to explain you what is Python and how to make you own cog, without looking at the Red's Documentation (But I will use it).
+# Welcome to a new era? (04/6/2020)
+Welcome everyone, here's a little update of what I made for abour 1 month.
+So I started working on a free Cleverbot cog, and it actually work, some cleanup and I will push it to the repository soon.
 
-That's all, stay home!
+About the Learning cog, it reached EoL, because at first, I (again) losed interest in doing it and I got some complication 
+in doing it (Like functions for checking if the level is complete.). However I'll be glad to help you with little Python/cog
+code issue (I don't replace a support channel!)
+
+Sorry for those who was expecting a lot from this cog
 
 ## Available cogs list
 
 | Cog Name     | Description                                          | Version    |
 | ------------ | ---------------------------------------------------- | ---------- |
 | Coronavirus  | Get stats about the Coronavirus.                     | Finished   |
-| Learning.    | Receive some tutorials about Python and cog creation | Beta (v01) |
+| Learning     | Receive some tutorials about Python.                 | EoL        |
+
+## Hidden cogs
+
+| Cog Name     | Description                                          | Version    |
+| ------------ | ---------------------------------------------------- | ---------- |
+| Learning     | Receive some tutorials about Python.                 | EoL        |
 
 ## Support
 
-Contact me at `Predeactor ✨#0495`. I will be glad to help you.
+Contact me at `Predeactor#0495`. I will be glad to help you.
 
 If you're having any important(s) issue(s), open an issue or pull request to this repo.
 
 ## Soon
 
-Working on an adventure cog, where you should help someone.
-Also a cog to help you learn Python and making Red cogs, isn't that your dream?
+Finishing a free Cleverbot cog.
--- learning/learning.py
+++ learning/learning.py
@@ -6,27 +6,54 @@ from redbot.core import commands, Config
 from .lessons import *
 
 class Learning(commands.Cog):
-    """This cog is made for learning the Python basics and how to make your own cog on Red."""
+    
+    """This cog is made for learning the Python basics and how to make your own cog on Red.
+    
+    Cog has reached EoL (End of Life), it mean that I won't give support, enhancement,
+     new lessons or anything else to this cog. However, you can open a pull request
+     and do change by yourself."""
+
+    __author__ = ['Predeactor']
+    __version__ = 'Last - EoL'
+
     def __init__(self, bot):
         self.bot = bot
         self.data = Config.get_conf(self, identifier=495954054)
 
         self.data.register_user(
-            lvl1=True,
-            lvl2=False
+            lvl1=False,
+            lvl2=False,
+            lvl3=False
         )
 
         super().__init__()
 
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """Thanks Sinbad!"""
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\nAuthor: {', '.join(self.__author__)}\nCog Version: {self.__version__}"
+
     @commands.group(name="learningpython")
     async def lpy(self, ctx: commands.GuildContext):
-        """Here, you will learn the Python basics."""
+        """Here, you will learn the Python's basics."""
         pass
+
+    @lpy.command(name="introduction")
+    async def intro(self, ctx: commands.Context):
+        """Introduction first of all all."""
+        intro_list = pintro()
+        for data in intro_list:
+            if type(data) is int:
+                await asyncio.sleep(data)
+            else:
+                embed = discord.Embed(title="Introduction", color=await self.bot.get_embed_colour(ctx), description=data)
+                await ctx.send(embed=embed)
+        await self.data.user(ctx.author).lvl1.set(True)
 
     @lpy.command(name="references")
     async def ref(self, ctx: commands.Context):
-        """Show you some useful links to know Python."""
-        refs_list = ref()
+        """Some useful links to learn more on Python."""
+        refs_list = pref()
         for data in refs_list:
             if type(data) is int:
                 await asyncio.sleep(data)
@@ -34,13 +61,15 @@ class Learning(commands.Cog):
                 embed = discord.Embed(title="References", color=await self.bot.get_embed_colour(ctx), description=data)
                 await ctx.send(embed=embed)
 
-
     @lpy.command(name="level1")
     async def lv1(self, ctx: commands.Context):
-        """Start the level 1.
+        """Start level 1.
         
         Let's learn how to install Python and run it."""
-        datas_list = lvl1()
+        if await self.data.user(ctx.author).lvl1() is False:
+            await ctx.send("Please read the introduction first of all.")
+            return
+        datas_list = plvl1()
         for data in datas_list:
             if type(data) is int:
                 await asyncio.sleep(data)
@@ -57,12 +86,28 @@ class Learning(commands.Cog):
         if await self.data.user(ctx.author).lvl2() is False:
             await ctx.send("Uh oh, seem you didn't even completed the level 1. I won't let you learn variables if you don't know how to launch Python.")
             return
-        datas_list = lvl2()
+        datas_list = plvl2()
         for data in datas_list:
             if type(data) is int:
                 await asyncio.sleep(data)
             else:
                 embed = discord.Embed(title="Variables", color=await self.bot.get_embed_colour(ctx), description=data)
                 await ctx.send(embed=embed)
-        #await self.data.user(ctx.author).lvl3.set(True) 
-    
+        await self.data.user(ctx.author).lvl3.set(True) 
+
+    @lpy.command(name="level3")
+    async def lv3(self, ctx: commands.Context):
+        """Start the level 3.
+        
+        Let's learn the differents datas type."""
+        if await self.data.user(ctx.author).lvl3() is False:
+            await ctx.send("Uh oh, seem you didn't even completed the level 2.")
+            return
+        datas_list = plvl3()
+        for data in datas_list:
+            if isinstance(data, int):
+                await asyncio.sleep(data)
+            else:
+                embed = discord.Embed(title="Type of Datas", color=await self.bot.get_embed_colour(ctx), description=data)
+                await ctx.send(embed=embed)
+        # await self.data.user(ctx.author).lvl4.set(True)
